aaa67e8901ab1f0437314784f2f1e3c85a3b1f92
Nicholas Ruunu <nicholas@ruu.nu>
2015-02-01 23:11:25 +0100

    Implements a card that matches another card

---
+++ spec/Memory/Card/CardSpec.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace spec\Memory\Card;
+
+use Memory\Card\Card;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use SplFileInfo;
+use Symfony\Component\Filesystem\Filesystem;
+
+class CardSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $fs = new Filesystem();
+        $fs->touch('/tmp/image.png');
+        $fs->touch('/tmp/another_image.png');
+        $image = new SplfileInfo('/tmp/image.png');
+        $this->beConstructedWith($image);
+    }
+
+    function it_matches_another_card_with_same_image()
+    {
+        $image = new SplFileInfo('/tmp/image.png');
+        $card = new Card($image);
+        $this->matches($card)->shouldReturn(true);
+    }
+
+    function it_does_not_match_card_with_another_image()
+    {
+        $image = new SplFileInfo('/tmp/another_image.png');
+        $card = new Card($image);
+        $this->matches($card)->shouldReturn(false);
+    }
+
+    function it_returns_real_path_to_the_image()
+    {
+        $realPath = realpath('/tmp/image.png');
+        $this->getImage()->shouldReturn($realPath);
+    }
+}
--- src/Memory/Card/Card.php
+++ src/Memory/Card/Card.php
@@ -2,11 +2,34 @@
 
 namespace Memory\Card;
 
-interface Card
+use SplFileInfo;
+
+class Card
 {
-  /**
-   * @param \Memory\Card\Card $card
-   * @return bool
-   */
-  public function matches(Card $card);
+    private $image;
+
+    /**
+     * @param \SplFileInfo $image
+     */
+    public function __construct(SplFileInfo $image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @param \Memory\Card\Card $card
+     * @return bool
+     */
+    public function matches(Card $card)
+    {
+        return $card->getImage() === $this->getImage();
+    }
+
+    /**
+     * @return string $image real path of image
+     */
+    public function getImage()
+    {
+        return $this->image->getRealPath();
+    }
 }
