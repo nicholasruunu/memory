4a216f271155211552f95ab4110e4a785f8a94c0
Nicholas Ruunu <nicholas@ruu.nu>
2015-02-02 02:10:58 +0100

    Deck can't turn card that has been removed

--- spec/Memory/Deck/DeckSpec.php
+++ spec/Memory/Deck/DeckSpec.php
@@ -2,13 +2,30 @@
 
 namespace spec\Memory\Deck;
 
+use Memory\Deck\Card;
+use Memory\Deck\CardCollection;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class DeckSpec extends ObjectBehavior
 {
+    function let(CardCollection $cardCollection, Card $card)
+    {
+        $cards = array($card, $card, $card, $card);
+        $cardCollection->getCards()->willReturn($cards);
+
+        $this->beConstructedWith($cardCollection);
+    }
+
     function it_can_turn_a_card_by_the_position_in_the_deck()
     {
         $this->turn($position = 0)->shouldHaveType('\Memory\Deck\Card');
+    }
+
+    function it_can_not_turn_cards_that_has_been_removed()
+    {
+        $this->remove(0, 1);
+        $this->shouldThrow(\Memory\Deck\Exceptions\CardAlreadyRemoved::class)->duringTurn(0);
+        $this->shouldThrow(\Memory\Deck\Exceptions\CardAlreadyRemoved::class)->duringTurn(1);
     }
 }
--- src/Memory/Deck/Deck.php
+++ src/Memory/Deck/Deck.php
@@ -2,11 +2,30 @@
 
 namespace Memory\Deck;
 
+use Memory\Deck\Exceptions\CardAlreadyRemoved;
+
 class Deck
 {
 
-    public function turn($argument1)
+    private $cards;
+
+    public function turn($position)
     {
+        if ( ! isset($this->cards[$position])) {
+            throw new CardAlreadyRemoved('Already Removed');
+        }
+
         return new Card(new \SplFileInfo(__FILE__));
+    }
+
+    public function remove($firstPosition, $secondPosition)
+    {
+        unset($this->cards[$firstPosition]);
+        unset($this->cards[$secondPosition]);
+    }
+
+    public function __construct(CardCollection $cardCollection)
+    {
+        $this->cards = $cardCollection->getCards();
     }
 }
